f788515acf829134d2f0b60193879ac907e9a404
Diamond Lee <diamolee55@gmail.com>
2021-04-08 16:43:32 -0400

    yea

--- assets/script.js
+++ assets/script.js
@@ -37,14 +37,48 @@ console.log(data);
 });
 
 
-let currLocation = document.querySelector('.weather-content__overview');
-let currTemp = document.querySelector('.weather-content__temp');
-let forecast = document.querySelector('.component__forecast-box');
+const CURRENT_LOCATION = document.getElementsByClassName('weather-content__overview')[0];
+const CURRENT_TEMP = document.getElementsByClassName('weather-content__temp')[0];
+const FORECAST = document.getElementsByClassName('component__forecast-box')[0]
 
 button.addEventListener('click',function getWeatherData() {
     fetch('https://api.openweathermap.org/data/2.5/forecast?q='+input.value+'&cnt=5&units=imperial&appid=e625f24b32e08b4026e29e46f076673d') 
     .then(response => response.json())
     .then(data => {
     console.log(data)
-  });
 });
+getWeatherData().then(data => {
+  let city = data.city.name;
+  let dailyForecast = data.list;
+  
+    renderData(city, dailyForecast);
+  });
+  renderData = (location, forecast) => {
+    const currentWeather = forecast[0].weather[0];
+    const widgetHeader =
+    `<h1>${location}</h1><small>${currentWeather.description}</small>`;
+  
+    CURRENT_TEMP.innerHTML =
+    `<i class="wi ${applyIcon(currentWeather.icon)}"></i>
+    ${Math.round(forecast[0].temp.day)} <i class="wi wi-degrees"></i>`;
+  
+    CURRENT_LOCATION.innerHTML = widgetHeader;
+  
+    // render each daily forecast
+    forecast.forEach(day => {
+      let date = new Date(day.dt * 1000);
+      let days = ['Sun', 'Mon', 'Tues', 'Wed', 'Thurs', 'Fri', 'Sat'];
+      let name = days[date.getDay()];
+      let dayBlock = document.createElement("div");
+      dayBlock.className = 'forecast__item';
+      dayBlock.innerHTML =
+        `<div class="forecast-item__heading">${name}</div>
+        <div class="forecast-item__info">
+        <i class="wi ${applyIcon(day.weather[0].icon)}"></i>
+        <span class="degrees">${Math.round(day.temp.day)}
+        <i class="wi wi-degrees"></i></span></div>`;
+      FORECAST.appendChild(dayBlock);
+    });
+  }
+});
+
